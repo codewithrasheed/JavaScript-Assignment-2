efddb1b576071e638740982fcf4c06b042542c1c
codewithrasheed <rasheedahmed4845@gmail.com>
2022-11-03 00:20:34 +0500

    Console Programming

---
+++ index.js
@@ -0,0 +1,45 @@
+// Chap 9 to 14 Practice
+// console.log("---------------------------------------")
+// console.log("Marksheet Assignment")
+// console.log("---------------------------------------")
+// var Subject1 =  +prompt("Maths Marks /100")
+// var Subject2 =  +prompt("Physics Marks /100")
+// var Subject3 =  +prompt("Computer Marks /100")
+// var Subject4 =  +prompt("Chemistry Marks /100")
+// var Subject5 =  +prompt("English Marks /100")
+// var Subject6 =  +prompt("Urdu Marks /100")
+// console.log("Maths Marks:", Subject1);
+// console.log("Physics Marks:", Subject2);
+// console.log("Computer Marks:", Subject3);
+// console.log("Chemistry Marks:", Subject4);
+// console.log("English Marks:", Subject5);
+// console.log("Urdu Marks:", Subject6);
+// var total = Subject1 + Subject2 + Subject3 + Subject4 + Subject5 + Subject6; 
+// console.log("Total Obtained Marks", total);
+// console.log("Total Marks: 600")
+// var percentage = total / 600 * 100;
+// console.log("Percentage:", percentage);
+
+// if(percentage >= 80 && percentage <= 100){
+//     console.log("Congrats! you have secured: A+ Grade");
+// }
+// else if(percentage >= 70 && percentage <= 80){
+//     console.log("Congrats! you have secured: A Grade");
+// }
+// else if(percentage >= 60 && percentage <= 70){
+//     console.log("Congrats! you have secured: B Grade");
+// }
+// else if(percentage > 50 && percentage <= 60){
+//     console.log("You have secured: C Grade");
+// }
+// else if(percentage > 40 && percentage <= 50){
+//     console.log("You have secured: D Grade");
+// }
+// else{
+//     console.log("You've Failed");
+// }
+// console.log("--------------------------------------")
+// Chap 15 (Arrays) Practice
+// var biodata = ["Name:", "Rasheed Ahmed", "Class:", "XII CS", "College:", "Sir Adamjee Institute", "Age", "17"];
+// console.log(biodata)
+// console.log("--------------------------------------")
